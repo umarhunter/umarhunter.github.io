758f29d1a63b297938f0fffb654e0634150f8378
umarhunter <88463513+umarhunter@users.noreply.github.com>
2025-03-28 19:59:54 +0000

    Deploying to gh-pages from @ umarhunter/umarhunter.github.io@1f8eadf924fe0690c0395926f2066c468da732b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "me",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
